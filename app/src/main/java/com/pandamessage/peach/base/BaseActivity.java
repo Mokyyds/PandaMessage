c31c19197f44468f4fb38b0c63ebefe62039296d
Peach <568797992@qq.com>
2016-03-17 13:26:52 +0800

    Initial commit

---
+++ app/src/main/java/com/pandamessage/peach/base/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.pandamessage.peach.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Peach on 2016/3/17.
+ */
+public class BaseActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/com/pandamessage/peach/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pandamessage.peach.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.pandamessage.peach.pandamessage.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
